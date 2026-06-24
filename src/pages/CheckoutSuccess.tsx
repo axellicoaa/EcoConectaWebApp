5096a417eebca0f8a52f4f43084771a58e936481
axellicoaa <axel.licoam@ug.edu.ec>
2026-06-24 00:01:09 -0500

    ajuste de formato react

--- src/pages/CheckoutSuccess.tsx
+++ src/pages/CheckoutSuccess.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Link, useSearchParams } from 'react-router-dom'
 import { CheckCircle, Home, Package, Leaf } from 'lucide-react'
 import { Button } from '../components/ui/Button'
